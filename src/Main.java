6716ddefbc6a2eb3dc22e7f1c26153601267f5d7
kimmingyun <mingyun8646@nate.com>
2023-05-30 14:31:55 +0900

    feat : 369게임과 윤년구하기를 MyUtils로 이동

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1700;
-        if ( checkLeapYear(i) )
-            System.out.println(i + " O");
-        else
-            System.out.println(i + " X");
-    }
-
-    public static boolean checkLeapYear(int year) {
-        return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ;
-    }
-
-    public static boolean isLeapYear(int year) {
-        if (year % 400 == 0) return true;
-        if (year % 100 == 0) return false;
-        if (year % 4 == 0) return true;
-        return false;
+        MyUtils.game369EX();
     }
 }
-
 
